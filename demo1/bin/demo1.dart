4f4b0b623dd65690c19c81e986efa8ccef68a346
The Coding Hive <nidhinsasankan@gmail.com>
2022-07-12 13:38:51 +0530

    initial commit

---
+++ demo1/bin/demo1.dart
@@ -0,0 +1,17 @@
+import 'package:demo1/demo1.dart' as demo1;
+
+void main() {
+  String name = "Nidhin";
+  int age = 20;
+  String email = "nidhinsasankan@gmail.com";
+  double cgpa = 8.3;
+  int a = 10;
+  int b = 20;
+
+  print("My Name is ${name}");
+  print("I am ${age} old");
+  print("My email is ${email}");
+  print("I have ${cgpa} cgpa");
+  print("the sum of ${b} and ${a} is ${b-a}");
+
+}
